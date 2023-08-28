e3a406b2a05e07752dad84750032f89d9e65bd5d
Braynner Marques <braynnermarques@hotmail.com>
2023-08-27 23:40:05 -0300

    1007 concluido

---
+++ 1007.js
@@ -0,0 +1,9 @@
+//obtem o primeiro valor do arquivo stdin
+let a = parseFloat(lines[0]);
+//obtem o segundo valor do arquivo stdin
+let b = parseFloat(lines[1]);
+let c = parseFloat(lines[2]);
+let d = parseFloat(lines[3]);
+let diferenca = (a*b-c*d)
+
+console.log("DIFERENCA = "+(diferenca).toFixed(0))

595326c45e4210dba2d05b62b3159bd87c122b70
Braynner Marques <braynnermarques@hotmail.com>
2023-09-01 23:42:22 -0300

    1019 concluido

---
+++ 1019.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8');
+var lines = input.split('\r\n');
+
+/////////////////////// COLAR NO BEECROWD O CODIGO DESTE PONTO EM DIANTE ///////////////////
+
+//quebrando a entrada de dados de uma linha em variaveis separadas
+//quando os valores estiverem na mesma linha
+//lines = lines[0].split(" ");
+
+//obtem o primeiro valor do arquivo stdin
+let tempo = parseInt(lines[0]);
+let minuto = parseInt(tempo/60)%60;
+let segundo = tempo%60;
+let hora = parseInt(tempo/3600);
+console.log(hora+":"+minuto+":"+segundo);

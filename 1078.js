21f418bba417f907b24e7153ea57f6e12b880e8f
Braynner Marques <braynnermarques@hotmail.com>
2023-09-17 15:15:22 -0300

    1078 concluido

---
+++ 1078.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8');
+var lines = input.split('\r\n');
+
+/////////////////////// COLAR NO BEECROWD O CODIGO DESTE PONTO EM DIANTE ///////////////////
+
+//quebrando a entrada de dados de uma linha em variaveis separadas
+//quando os valores estiverem na mesma linha
+//lines = lines[0].split(" ");
+
+//obtem o primeiro valor do arquivo stdin
+
+let n = parseInt(lines[0]);
+
+if (n>2 && n<1000) {
+    for (let index = 1; index <= 10; index++) {
+        console.log(index+" x "+n+" = "+index*n);        
+    }
+}

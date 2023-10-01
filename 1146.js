3e6d8fd1f13f7fc057c90d91c63b082d4a4cf78c
Braynner Marques <braynnermarques@hotmail.com>
2023-10-01 17:20:18 -0300

    1146 concluido

---
+++ 1146.js
@@ -0,0 +1,25 @@
+const { type } = require('os');
+const path = require('path');
+const { stringify } = require('querystring');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8');
+var lines = input.split('\r\n');
+
+/////////////////////// COLAR NO BEECROWD O CODIGO DESTE PONTO EM DIANTE ///////////////////
+
+//quebrando a entrada de dados de uma linha em variaveis separadas
+//quando os valores estiverem na mesma linha
+//lines = lines[0].split(" ");
+
+//obtem o primeiro valor do arquivo stdin
+let i = 0 ;
+do {
+    let saida = "";    
+    for (let index = 1; index <= lines[i]; index++) {
+        saida += index + " ";
+    }
+    console.log(saida.trim());
+    i++    
+} while (lines[i] != 0)

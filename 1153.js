6eb25331b51888b0bfe9d913ee80ea9753434717
Braynner Marques <braynnermarques@hotmail.com>
2023-10-05 21:41:30 -0300

    1153 concluido

---
+++ 1153.js
@@ -0,0 +1,23 @@
+const { type } = require('os');
+const path = require('path');
+const { stringify } = require('querystring');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8');
+var lines = input.split('\r\n');
+
+/////////////////////// COLAR NO BEECROWD O CODIGO DESTE PONTO EM DIANTE ///////////////////
+
+//quebrando a entrada de dados de uma linha em variaveis separadas
+//quando os valores estiverem na mesma linha
+//lines = lines[0].split(" ");
+
+//obtem o primeiro valor do arquivo stdin
+
+let N = parseInt(lines[0])
+let fatorial = N
+for (let index = 1; index < N; index++) {
+    fatorial *=(N-index)    
+}
+console.log(fatorial);

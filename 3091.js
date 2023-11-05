51764e1cabb2d7f13b7fcd125e8cc2edd37b58e0
Braynner Marques <braynnermarques@hotmail.com>
2023-11-05 20:07:24 -0300

    3091 concluido

---
+++ 3091.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8');
+var lines = input.split('\r\n');
+
+let a = parseInt(lines[0]);
+let b = parseInt(lines[1]);
+
+console.log(a%b);

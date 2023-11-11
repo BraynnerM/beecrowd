179b9ab9f02ee15a2220eb4f1d77db16a23d96b9
Braynner Marques <braynnermarques@hotmail.com>
2023-11-10 21:16:20 -0300

    3145 concluido

---
+++ 3145.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8');
+var lines = input.split('\r\n');
+
+/////////////////////// COLAR NO BEECROWD O CODIGO DESTE PONTO EM DIANTE ///////////////////
+
+//quebrando a entrada de dados de uma linha em variaveis separadas
+//quando os valores estiverem na mesma linha
+lines = lines[0].split(" ");
+let N = parseInt(lines[0]);
+let X = parseInt(lines[1]);
+console.log((X/(N+2)).toFixed(2));

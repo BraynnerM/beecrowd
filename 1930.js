fb669db59adf323839ccae2dbb5ceaacaaadce8b
Braynner Marques <123399239+BraynnerM@users.noreply.github.com>
2023-11-20 14:34:42 -0300

    1930 concluido

---
+++ 1930.js
@@ -0,0 +1,3 @@
+let vetorReguas = lines[0].split(" ")
+let somaTomadas= parseInt(vetorReguas[0])+parseInt(vetorReguas[1])+parseInt(vetorReguas[2])+parseInt(vetorReguas[3])
+console.log(somaTomadas-3)

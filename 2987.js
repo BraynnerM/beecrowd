ec24da549a0524f6dfa787f1325b3eff32bfad84
Braynner Marques <123399239+BraynnerM@users.noreply.github.com>
2023-12-04 18:49:42 -0300

    2987 concluido

---
+++ 2987.js
@@ -0,0 +1,10 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+ let texto = lines[0]
+console.log((texto.charCodeAt(0))-64)

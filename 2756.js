010b3dea5a72fcd2e353443dc72dd2787c55564b
Braynner Marques <123399239+BraynnerM@users.noreply.github.com>
2023-12-08 18:42:46 -0300

    2756 concluido

---
+++ 2756.js
@@ -0,0 +1,17 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+console.log("       A")
+console.log("      B B")
+console.log("     C   C")
+console.log("    D     D")
+console.log("   E       E")
+console.log("    D     D")
+console.log("     C   C")
+console.log("      B B")
+console.log("       A")

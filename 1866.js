a54ad03181e6e76694021a37655ffa222b5366f2
Braynner Marques <braynnermarques@hotmail.com>
2023-12-14 23:20:07 -0300

    1866 concluido

---
+++ 1866.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const currentFile = path.dirname(process.argv[1]);
+
+//realiza a leitura das linhas no arquivo dev\stdin
+var input = require('fs').readFileSync(currentFile + '\\dev\\stdin', 'utf8');
+var lines = input.split('\r\n');
+
+/////////////////////// COLAR NO BEECROWD O CODIGO DESTE PONTO EM DIANTE ///////////////////
+
+//quebrando a entrada de dados de uma linha em variaveis separadas
+//quando os valores estiverem na mesma linha
+//lines = lines[0].split(" ");
+
+//obtem o primeiro valor do arquivo stdin
+let qtdeEntr = parseInt(lines[0]);
+for (let i = 0; i < qtdeEntr; i++) {
+    let entrada = parseInt(lines[i+1])
+    let S=0;
+    for (let i = 0; i < entrada; i++){
+        S = S+1
+        i++
+        if (i < entrada) {
+            S = S-1            
+        }
+    }
+    console.log(S);
+    
+}
